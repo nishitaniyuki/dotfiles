32a7b76d96dbe6c0fc19083309b23ec5ed3167b0
nishitani yuki <yuki.nishitani85@gmail.com>
2017-04-30 09:42:00 +0900

    add zsh-completions

--- .zsh/zplug.zsh
+++ .zsh/zplug.zsh
@@ -5,5 +5,7 @@ zplug "~/.zsh", from:local, use:"<->_*.zsh"
 
 zplug "mafredri/zsh-async", from:github
 zplug "sindresorhus/pure", use:pure.zsh, from:github, as:theme
+
+zplug "zsh-users/zsh-completions"
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 
