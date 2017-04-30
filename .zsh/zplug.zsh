26727c0cfb7ef8e9c04deb94e6d3aef0cc794655
nishitani yuki <yuki.nishitani85@gmail.com>
2017-04-30 09:42:34 +0900

    add zsh-autosuggestions

--- .zsh/zplug.zsh
+++ .zsh/zplug.zsh
@@ -6,6 +6,7 @@ zplug "~/.zsh", from:local, use:"<->_*.zsh"
 zplug "mafredri/zsh-async", from:github
 zplug "sindresorhus/pure", use:pure.zsh, from:github, as:theme
 
+zplug "zsh-users/zsh-autosuggestions"
 zplug "zsh-users/zsh-completions"
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 
