c261476edeca2829e3d9e418a11de6561c9c28ba
nishitaniyuki <yuki.nishitani85@gmail.com>
2018-07-28 23:42:13 +0900

    bug fix

--- cookbooks/nodejs/default.rb
+++ cookbooks/nodejs/default.rb
@@ -2,5 +2,5 @@ NVM_VERSION = 'v0.33.11'
 
 execute "curl -o- https://raw.githubusercontent.com/creationix/nvm/#{NVM_VERSION}/install.sh | bash" do
   user node[:user]
-  not_if "test -f #{ENV['HOME']}/.nvm/README.markdown"
+  not_if "test -f #{ENV['HOME']}/.nvm/README.md"
 end
