948717ecfe9e33a736d3e056f3c03971d842b35c
nishitaniyuki <yuki-nishitani@cybozu.co.jp>
2018-07-30 02:40:56 +0000

    bug fix

--- cookbooks/ruby/default.rb
+++ cookbooks/ruby/default.rb
@@ -1,4 +1,4 @@
 execute "wget -q https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-installer -O- | bash" do
   user node[:user]
-  not_if "test -f #{ENV['HOME']}/.rbenv/version"
+  not_if "test -f #{ENV['HOME']}/.rbenv/README.md || test -f ~/.rbenv/README.md"
 end
