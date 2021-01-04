4c5bb68ef355bb68147a38be4e900c1e4ae229ad
nishitaniyuki <yuki-nishitani@cybozu.co.jp>
2021-01-04 13:44:07 +0000

    bug fix

--- cookbooks/k8s/default.rb
+++ cookbooks/k8s/default.rb
@@ -1,5 +1,5 @@
 # TODO: support darwin
-execute "curl -o #{ENV['HOME']}/bin/kubectl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl" do
+execute "curl -o #{ENV['HOME']}/bin/kubectl --create-dirs -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl" do
   user node[:user]
   not_if "test -f #{ENV['HOME']}/bin/kubectl"
 end
