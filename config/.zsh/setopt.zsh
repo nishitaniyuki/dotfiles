5eb67f84750ff2f708e8dab13fc725e735bb3f13
nishitaniyuki <yuki-nishitani@cybozu.co.jp>
2021-01-11 12:04:55 +0000

    add setopt.zsh

---
+++ config/.zsh/setopt.zsh
@@ -0,0 +1,9 @@
+# ref: https://leetschau.github.io/remove-duplicate-zsh-history.html
+setopt EXTENDED_HISTORY
+setopt HIST_EXPIRE_DUPS_FIRST
+setopt HIST_IGNORE_DUPS
+setopt HIST_IGNORE_ALL_DUPS
+setopt HIST_IGNORE_SPACE
+setopt HIST_FIND_NO_DUPS
+setopt HIST_SAVE_NO_DUPS
+setopt HIST_BEEP
