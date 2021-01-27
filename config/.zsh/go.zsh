1018deee0a3fa45396cf576cad6968a4478bc076
nishitaniyuki <yuki-nishitani@cybozu.co.jp>
2021-01-27 12:25:24 +0000

    GO111MODULE on

--- config/.zsh/go.zsh
+++ config/.zsh/go.zsh
@@ -1,4 +1,5 @@
 export GOVERSION="$(cat ${HOME}/.go/.goversion)"
 export GOROOT="${HOME}/.go/${GOVERSION}"
 export GOPATH="${HOME}"
+export GO111MODULE="on"
 export PATH="${GOROOT}/bin:${GOPATH}/bin:${PATH}"
