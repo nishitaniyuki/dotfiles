6e3f8946053bd3db313379a87bdd65b2da9be644
nishitaniyuki <yuki-nishitani@cybozu.co.jp>
2021-03-31 14:17:02 +0000

    update

--- config/.zsh/k8s.zsh
+++ config/.zsh/k8s.zsh
@@ -6,3 +6,12 @@ k() {
   fi
 }
 compdef k=kubectl
+
+k3() {
+  if [[ $# -gt 0 ]]; then
+    kubectl --kubeconfig /etc/rancher/k3s/k3s.yaml "$@"
+  else
+    kubectl --kubeconfig /etc/rancher/k3s/k3s.yaml config current-context
+  fi
+}
+compdef k3=kubectl
--- config/.zsh/misc.zsh
+++ config/.zsh/misc.zsh
@@ -1,2 +1,5 @@
 export PLANTUML_LIMIT_SIZE=16384
 export PATH="${PATH}:${HOME}/bin"
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
